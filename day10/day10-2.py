090df3df16b300365a9c749dc8f6c18fd1ff1a39
Aurel-Dragos Hofnar <aurel-dragos@hofnar.ro>
2016-12-12 22:28:33 +0200

    don\'t have time for this right now ... solved it on the paper tbh ...

--- day10/day10-2.py
+++ day10/day10-2.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python
 
-import collections
-import sys
-from collections import defaultdict
-
-
 instrunctions = {}
 values = {}
 outputs = {}
 
+# each line is parsed to populate the dicts instructions and values
 def processLine(line):
 	global instrunctions, values
 	split = line.split(' ')
+	# if line starts with 'bot, it means it's an instruction for a bot
 	if split[0] == 'bot':
 					#bot gives    bot/out   number    bot/out    number
 		instrunctions[int(split[1])] = split[5], int(split[6]), split[10], int(split[11])
+	# else it's an instruction that says that a value goes to a bot
 	else:
 		#       value        bot
 		values[int(split[1])] = int(split[5])
