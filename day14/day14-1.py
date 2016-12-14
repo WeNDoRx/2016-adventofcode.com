e8eb3c2592b4f7f4540b36efe4465bc57195ee89
Aurel-Dragos Hofnar <aurel-dragos@hofnar.ro>
2016-12-14 12:51:55 +0200

    day 14

--- day14/day14-1.py
+++ day14/day14-1.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python
+
+import re
+import md5
+
+pattern1 = re.compile(".*?(?P<A>.)(?P=A){2}.*")
+pattern2 = re.compile(".*?(?P<A>.)(?P=A){4}.*")
+
+hashes = {}
+found = []
+
+with open('day14.input', 'r') as f:
+    salt = f.readline().rstrip("\n")
+
+i = 0
+
+while len(found) < 64:
+	m = md5.new()
+	m.update(salt+str(i))
+	if pattern1.match(m.hexdigest()):
+		for x in xrange(i + 1, i + 1000):
+			n = md5.new()
+			n.update(salt+str(x))
+			if pattern2.match(n.hexdigest()) and pattern1.match(m.hexdigest()).group('A') == pattern2.match(n.hexdigest()).group('A'):
+				#print m.hexdigest(), pattern1.match(m.hexdigest()).group('A'), i, n.hexdigest(), x
+				found += [i]
+				break
+	i += 1
+
+print found[64 - 1]
--- day14/day14-2.py
+++ day14/day14-2.py
@@ -1,1 +1,48 @@
 #!/usr/bin/env python
+
+import re
+import md5
+import sys
+
+pattern1 = re.compile(".*?(?P<A>.)(?P=A){2}.*")
+pattern2 = re.compile(".*?(?P<A>.)(?P=A){4}.*")
+
+hashes = {}
+found = []
+
+with open('day14.input', 'r') as f:
+    salt = f.readline().rstrip("\n")
+
+i = 0
+
+def keyStretch(hexx, times):
+	string = hexx.hexdigest()
+	for stretch in xrange(0, times):
+		a = md5.new()
+		a.update(string)
+		string = a.hexdigest()
+	return a
+
+# populate the hashes dict with the first 1000 extended hashes
+for x in xrange(0, 1000):
+	m = md5.new()
+	m.update(salt+str(x))
+	hashes[x] = keyStretch(m, 2016).hexdigest()
+
+while len(found) < 64:
+	extended_hash = hashes[i]
+
+	tmp = md5.new()
+	tmp.update(salt+str(i + 1000))
+
+	hashes[1000 + i] = keyStretch(tmp, 2016).hexdigest()
+
+	if pattern1.match(extended_hash):
+		for x in xrange(i + 1, i + 1000):
+			if pattern2.match(hashes[x]) and pattern1.match(hashes[i]).group('A') == pattern2.match(hashes[x]).group('A'):
+				#print hashes[i], pattern1.match(hashes[i]).group('A'), i, hashes[x], x
+				found += [i]
+				break
+	i += 1
+
+print found[64 - 1]
