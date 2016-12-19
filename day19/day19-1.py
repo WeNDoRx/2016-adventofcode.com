003bc86c8e02c84eab9f7186853d81f2d8c08698
Aurel-Dragos Hofnar <aurel-dragos@hofnar.ro>
2016-12-19 17:51:18 +0200

    temp

--- day19/day19-1.py
+++ day19/day19-1.py
@@ -1,1 +1,5 @@
 #!/usr/bin/env python
+
+with open('day19.input', 'r') as f:
+	line = f.readline().rstrip("\n")
+	print int( bin(int(line))[3:] + bin(int(line))[2:3], 2)
--- day19/day19-2.py
+++ day19/day19-2.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python
+
+with open('day19.input', 'r') as f:
+	line = int(f.readline().rstrip("\n"))
+	elves = range(5)
+	i = 0
+	while len(elves) > 1:
+		remove = len(elves[:i]) 
