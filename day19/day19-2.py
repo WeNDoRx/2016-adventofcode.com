3e89b39a6a9f1b48d8c487b7fa8fd5aa30614e0b
Aurel-Dragos Hofnar <aurel-dragos@hofnar.ro>
2016-12-20 23:41:41 +0200

    day 19 - part 2 not mine

--- day19/day19-2.py
+++ day19/day19-2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+from math import log
+
 with open('day19.input', 'r') as f:
 	line = int(f.readline().rstrip("\n"))
-	elves = range(5)
-	i = 0
-	while len(elves) > 1:
-		remove = len(elves[:i]) 
+	# way too smart solution from https://www.reddit.com/r/adventofcode/comments/5j4lp1/2016_day_19_solutions/dbdobax/
+	# just too beautiful to let it slip away ...
+	p = 3 ** int(log(line - 1, 3))
+	print line - p + max(line -2 * p, 0)
